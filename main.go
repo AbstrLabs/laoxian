1337c6bb8d02093e585a44f8aec095fc6385b9c3
Bo Yao <by677@nyu.edu>
2023-11-19 12:18:36 +0800

    initial commit

---
+++ main.go
@@ -0,0 +1,42 @@
+// Copyright 2022 The golang.design Initiative Authors.
+// All rights reserved. Use of this source code is governed
+// by a MIT license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+	"golang.design/x/clipboard"
+	"golang.design/x/hotkey"
+)
+
+func main() {
+	err := clipboard.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	w := app.New().NewWindow("golang.design/x/hotkey")
+
+	label := widget.NewLabel("Hello golang.design!")
+	button := widget.NewButton("Hi!", func() { label.SetText("Welcome :)") })
+	w.SetContent(container.NewVBox(label, button))
+
+	go func() {
+		// Register a desired hotkey.
+		hk := hotkey.New([]hotkey.Modifier{hotkey.ModCmd}, hotkey.KeyL)
+		if err := hk.Register(); err != nil {
+			panic("hotkey registration failed")
+		}
+		// Start listen hotkey event whenever it is ready.
+		for range hk.Keydown() {
+			ret := clipboard.Read(clipboard.FmtText)
+			label.SetText(string(ret))
+			w.Show()
+		}
+	}()
+
+	w.ShowAndRun()
+}
